f60651863f5675253eafd2d1de7d5b0272b4f140
carternadain <cnadain@gmail.com>
2022-04-08 20:22:19 -0700

    start api routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const saveData = require('../db/saveData');
+
+
+// get request
+router.get('/notes', (req, res) => {
+    
+  })
+
+
+
+
+// post request
+router.get('/notes', (req, res) => {
+    
+  })
+
+
+module.exports = router;
