2a5f94c7c1730d7cdbcff5749c2491cf9858b436
carternadain <cnadain@gmail.com>
2022-04-08 20:49:14 -0700

    api routes write post/get request

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,17 +3,23 @@ const saveData = require('../db/saveData');
 
 
 // get request
-router.get('/notes', (req, res) => {
-    
-  })
+router.get('/notes', function (req, res) {
+    saveData
+    .retrieveNotes()
+    .then(notes => res.json(notes))
+    .catch(err => res.status(500).json(err));
+  });
 
 
 
 
 // post request
 router.get('/notes', (req, res) => {
-    
-  })
+    saveData
+    .retrieveNotes()
+    .then(notes => res.json(notes))
+    .catch(err => res.status(500).json(err));
+  });
 
 
 module.exports = router;
